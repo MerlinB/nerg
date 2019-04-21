ce588b04089b275c3640f7036226b9ba8c37f972
Merlin Buczek <merlin.buczek@gmail.com>
2019-04-21 11:51:48 +0200

    Added Loop

--- src/Main.purs
+++ src/Main.purs
@@ -3,12 +3,14 @@ module Main where
 import Prelude
 
 import Data.Maybe (Maybe(..))
+import Data.Tuple (fst, snd)
 import Effect (Effect)
 import Effect.Console as Console
-import Graphics.Canvas (getCanvasElementById, getContext2D)
+import Graphics.Canvas (Context2D, getCanvasElementById, getContext2D)
 import Graphics.Drawing (render)
-import Grid.Internal (randomGrid)
-import Grid.Render (redrawGrid)
+import Grid (nextState)
+import Grid.Internal (randomGrid, Grid)
+import Grid.Render (drawUpdate, redrawGrid)
 
 main :: Effect Unit
 main = do
@@ -18,3 +20,10 @@ main = do
     Just canvas -> do
       ctx <- getContext2D canvas
       render ctx $ redrawGrid randomGrid
+      gridLoop ctx randomGrid
+
+gridLoop :: Context2D -> Grid -> Effect Unit
+gridLoop ctx grid = do
+  render ctx $ drawUpdate $ fst nextTuple
+  gridLoop ctx $ snd nextTuple where
+    nextTuple = nextState grid
